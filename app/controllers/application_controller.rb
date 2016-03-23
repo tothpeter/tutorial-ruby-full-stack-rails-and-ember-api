5bac7f7527106bed6939b0b7c8a59870d2363f77
Peter Toth <tothpeter08@gmail.com>
2016-03-23 22:04:37 +0100

    add relationship params helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,23 @@
 class ApplicationController < ActionController::API
+  def relationships_params
+    return @associations if @associations
+
+    @associations = {}
+
+    params["data"]["relationships"].each do |key, value|
+      if value["data"].is_a? Array
+        @associations[key.to_sym] = value["data"].map { |data| find_related_object data }
+      else
+        @associations[key.to_sym] = find_related_object value[:data]
+      end
+    end
+
+    @associations
+  end
+
+  private
+
+  def find_related_object data
+    data[:type].singularize.titlecase.constantize.find data[:id]
+  end
 end
