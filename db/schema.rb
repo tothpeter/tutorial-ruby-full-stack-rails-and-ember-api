ef8d6d61056cc4219a9b31305919319868f31483
Peter Toth <tothpeter08@gmail.com>
2016-03-28 17:53:46 +0200

    Add oauth_token model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160328121506) do
+ActiveRecord::Schema.define(version: 20160328160304) do
+
+  create_table "auth_tokens", force: :cascade do |t|
+    t.string   "email"
+    t.string   "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
