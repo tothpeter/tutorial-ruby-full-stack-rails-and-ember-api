74146f9d6f1d8484ab025dff5ebe1ed2946cad61
Peter Toth <tothpeter08@gmail.com>
2016-03-28 21:04:58 +0200

    Dive deep into google-api-client gem and fix the auth endpoint

--- app/controllers/authorization_controller.rb
+++ app/controllers/authorization_controller.rb
@@ -1,26 +1,30 @@
+require 'google/api_client/client_secrets'
+require 'google/apis/oauth2_v2'
+
 class AuthorizationController < ActionController::API
   def create
-    auth_client = Google::APIClient::ClientSecrets.new(web: {
+    client = Google::APIClient::ClientSecrets.new('web' => {
       client_id: ENV['GOOGLE_CLIENT_ID'],
       client_secret: ENV['GOOGLE_CLIENT_SECRET'],
-      redirect_url: 'http://localhost:4200/oauth2callback'
+      redirect_uri: 'http://localhost:4200/oauth2callback'
     }).to_authorization
 
-    auth_client.grant_type = 'authorization_code'
-    auth_client.code = params[':authorization_code']
 
-    token = auth_client.fetch_access_token!
+    client.grant_type = 'authorization_code'
+    client.code = params['authorization_code']
 
-    client = Google::Apis::Oauth2V2::Oauth2Service.new
-    client.authorization = auth_client
-    user_info = client.get_userinfo
+    token = client.fetch_access_token!
+
+    oauth_client = Google::Apis::Oauth2V2::Oauth2Service.new
+    oauth_client.authorization = client
+    user_info = oauth_client.get_userinfo
 
     auth_token = AuthToken.where(email: user_info.email).first_or_create
-    auth_token.update token: token
+    auth_token.update token: token['access_token']
 
     render json: {
       email: user_info.email,
-      token: token
+      token: token['access_token']
     }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   resources :projects
   resources :notes
   resources :offers
+
+  resources :authorization, only: [:create]
 end
